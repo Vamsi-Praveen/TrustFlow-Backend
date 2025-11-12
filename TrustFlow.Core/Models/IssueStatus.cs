8c4e0aa6e5f8160cd38ea36301b590171ef48c48
TSRCHARAN <tsr.charan@gmail.com>
2025-11-12 14:36:34 +0530

    Added IssueStatus

---
+++ TrustFlow.Core/Models/IssueStatus.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace TrustFlow.Core.Models
+{
+    public class IssueStatus : BaseEntity
+    {
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("Description")]
+        public string Description { get; set; }
+    }
+}
